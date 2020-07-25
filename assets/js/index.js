ec655784279060b345eed9e9b4c6678879463452
muskan278 <59742017+muskan278@users.noreply.github.com>
2020-07-25 14:36:11 +0530

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,7 +2,7 @@ $(func = function () {
       $(".blink").typed({
          strings: [" Think, We Innovate, We Create!"," Think", " Innovate"," Create!"],
           typeSpeed: 40, // typing speed
-          backDelay: 2000,
+          backDelay: 1000,
           callback: function (){
             func();
           }
